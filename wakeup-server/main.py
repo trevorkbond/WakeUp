71c22350c0a7e2fed2935edb4e8f2809bcd656c0
Trevor Bond <trevorkbond@gmail.com>
2025-11-26 18:46:02 +0000

    Add simple alarm server

--- wakeup-server/main.py
+++ wakeup-server/main.py
@@ -1,11 +1,19 @@
 # main.py
 import pygame
+import requests
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import FileResponse
 
 ALARM_PATH = "../alarm/alarm.mp3"
+WEBHOOK_URL = ""
+with open("webhook_url.txt", "r") as f:
+    WEBHOOK_URL = f.read().strip()
 
 pygame.mixer.init()
 app = FastAPI()
+
+requests.post(WEBHOOK_URL)
 
 @app.post("/start")
 async def start_alarm():
@@ -19,3 +27,13 @@ async def stop_alarm():
     if pygame.mixer.music.get_busy():
         pygame.mixer.music.stop()
     return {"message": "Stopped alarm"}
+
+@app.post("/speech")
+async def check_speech(request: Request):
+    data = await request.json() 
+    text = data.get("text", "")
+    print(text)
+
+@app.get("/")
+def read_index():
+    return FileResponse("index.html")
